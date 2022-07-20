908f6d7879d7160fd3a9b48d68736bb3d81d445e
Omeiza Alabi <omeizaalabi@gmail.com>
2022-07-20 14:26:13 +0100

    updating run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=22559900/machine-learning-microservice:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy machine-learning-microservice-kube --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/machine-learning-microservice-kube-5b9f8587b-w4dhw --address 0.0.0.0 8000:80
